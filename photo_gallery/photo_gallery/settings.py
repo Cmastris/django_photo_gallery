26d4f15e211db2eb85c86cb9b3290af3d1f1d1e7
Chris Mastris <cmastris@hotmail.com>
2022-07-20 19:55:08 +0100

    Add photos to `INSTALLED_APPS`

--- photo_gallery/photo_gallery/settings.py
+++ photo_gallery/photo_gallery/settings.py
@@ -28,10 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
 INSTALLED_APPS = [
+    'photos.apps.PhotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
