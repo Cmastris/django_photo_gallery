944d869da513fbcd7bf43df0e7e7f340a8d6e973
Chris Mastris <cmastris@hotmail.com>
2022-07-24 14:51:21 +0100

    Add Photo model to admin interface

--- photo_gallery/photos/admin.py
+++ photo_gallery/photos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Photo
 
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    # TODO: add photo upload text (at least 2000px wide, otherwise low visual quality)
+    # TODO: improve admin appearance
+    pass
+
+
+admin.site.register(Photo, PhotoAdmin)
