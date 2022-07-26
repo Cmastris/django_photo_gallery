b24685acb8c808cc0375a241dec3ae5cbc2033b1
Chris Mastris <cmastris@hotmail.com>
2022-07-26 21:10:42 +0100

    Replace `admin_thumbnail` with `thumbnail_img_tag`
    `thumbnail_img_tag` is required to add the thumbnail image
    to Photo detail pages, so might as well use it on listing
    pages as well rather than using a separate implementation

--- photo_gallery/photos/admin.py
+++ photo_gallery/photos/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from imagekit.admin import AdminThumbnail
 from .models import Photo
 
 
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'admin_thumbnail')
-    admin_thumbnail = AdminThumbnail(image_field='thumbnail')
+    list_display = ('__str__', 'thumbnail_img_tag')
 
-    # Display non-editable thumbnail
+    # Display a non-editable thumbnail on Photo detail pages
     readonly_fields = ['thumbnail_img_tag']
 
 
