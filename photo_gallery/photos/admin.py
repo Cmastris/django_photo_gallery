7f71cb786e6128a730790b1fde16028d39182d54
Chris Mastris <cmastris@hotmail.com>
2022-08-13 15:21:02 +0100

    Add country filter to Photo list admin interface

--- photo_gallery/photos/admin.py
+++ photo_gallery/photos/admin.py
@@ -24,6 +24,7 @@ class PhotoAdmin(admin.ModelAdmin):
     readonly_fields = ['thumbnail_img_tag']
 
     list_display = ('title', 'thumbnail_img_tag', 'slug')
+    list_filter = ['country']
 
     def get_fields(self, request, obj=None):
         """Return a list of fields (str) for the Photo add form (obj=None) or change form.
