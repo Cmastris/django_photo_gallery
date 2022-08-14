230807045d5f5202329d1a5383ed13194bdb606e
Chris Mastris <cmastris@hotmail.com>
2022-08-14 15:03:52 +0100

    Re-order models.py classes

--- photo_gallery/photos/models.py
+++ photo_gallery/photos/models.py
@@ -14,6 +14,27 @@ def validate_lowercase(string):
         raise ValidationError("All letters must be lowercase.")
 
 
+class Collection(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=5000)
+
+    slug_guidelines = "Enter a unique, descriptive URL path (e.g. based on the name) containing " \
+                      "only lowercase letters, numbers,  and hyphens (instead of spaces). "
+
+    slug = models.SlugField(max_length=50,
+                            unique=True,
+                            help_text=slug_guidelines,
+                            validators=[validate_slug, validate_lowercase])
+
+    published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
 class Country(models.Model):
     name = models.CharField(max_length=255, unique=True, validators=[MinLengthValidator(2)])
 
@@ -74,24 +95,3 @@ class Photo(models.Model):
 
     def __str__(self):
         return "{} ({})".format(self.title, self.slug)
-
-
-class Collection(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(max_length=5000)
-
-    slug_guidelines = "Enter a unique, descriptive URL path (e.g. based on the name) containing " \
-                      "only lowercase letters, numbers,  and hyphens (instead of spaces). "
-
-    slug = models.SlugField(max_length=50,
-                            unique=True,
-                            help_text=slug_guidelines,
-                            validators=[validate_slug, validate_lowercase])
-
-    published = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['name']
