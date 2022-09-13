9abc981b1a06b1784d76e9dedd973b1a65151a2b
Chris Mastris <cmastris@hotmail.com>
2022-09-13 18:40:16 +0100

    Define Photo model get_absolute_url()

--- photo_gallery/photos/models.py
+++ photo_gallery/photos/models.py
@@ -105,3 +105,6 @@ class Photo(models.Model):
 
     def __str__(self):
         return "{} ({})".format(self.title, self.slug)
+
+    def get_absolute_url(self):
+        return reverse("photo_detail", kwargs={"slug": self.slug})
