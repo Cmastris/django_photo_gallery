5e3dbd039f2bdffffa89d831fec73ecb37272a1d
Chris Mastris <cmastris@hotmail.com>
2022-10-12 21:56:53 +0100

    Add navigation models to the admin site

--- photo_gallery/nav/admin.py
+++ photo_gallery/nav/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import NavSection, NavLink
 
-# Register your models here.
+
+class NavLinkInline(admin.StackedInline):
+    # Display 1 inlined NavLink add form at a time, up to 8
+    model = NavLink
+    extra = 1
+    max_num = 8
+
+
+class NavSectionAdmin(admin.ModelAdmin):
+    inlines = [NavLinkInline]
+
+
+admin.site.register(NavSection, NavSectionAdmin)
