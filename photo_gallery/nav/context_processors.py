6d477e8203e77f5a9bbb71d77fc6c5a73c287969
Chris Mastris <cmastris@hotmail.com>
2022-10-15 15:08:28 +0100

    Add `prefetch_related()` to nav context processor

--- photo_gallery/nav/context_processors.py
+++ photo_gallery/nav/context_processors.py
@@ -4,7 +4,8 @@ from .models import NavSection
 def navigation(request):
     """Add all NavSection objects to template context data (via settings.py `TEMPLATES`).
     https://docs.djangoproject.com/en/4.0/ref/templates/api/#writing-your-own-context-processors
+    https://docs.djangoproject.com/en/4.0/ref/models/querysets/#prefetch-related
     """
     return {
-        "nav_sections": NavSection.objects.all(),
+        "nav_sections": NavSection.objects.prefetch_related('navlink_set').all(),
     }
