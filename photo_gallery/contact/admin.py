3540072059a798d662a87892657d55c26b65090b
Chris Mastris <cmastris@hotmail.com>
2022-10-28 12:31:27 +0100

    Add ContactMessage to the admin site

--- photo_gallery/contact/admin.py
+++ photo_gallery/contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    readonly_fields = ['first_name', 'last_name', 'email_address', 'subject', 'message',
+                       'contact_time']
+
+    list_display = ['subject', 'contact_time', 'responded_to', 'resolved']
+    list_filter = ['responded_to', 'resolved']
+
+    search_fields = ['email_address', 'subject', 'message']
+    search_help_text = "Search email addresses, subjects, and messages."
+
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
