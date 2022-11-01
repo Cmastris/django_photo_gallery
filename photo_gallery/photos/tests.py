c5d76870e798b37ef441a17109fa21686f2b3ed7
Chris Mastris <cmastris@hotmail.com>
2022-11-01 21:56:16 +0000

    Delete `MockPhotoImages` (no longer required)

--- photo_gallery/photos/tests.py
+++ photo_gallery/photos/tests.py
@@ -1,11 +1,8 @@
 import datetime
 from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.db import models
 from django.test import override_settings, RequestFactory, tag, TestCase
 from django.urls import reverse
-from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFit
 from pathlib import Path
 
 from photo_gallery.settings import BASE_DIR
@@ -18,20 +15,6 @@ def create_uploaded_file_object(image_path):
     return SimpleUploadedFile(name='test_image.jpg',
                               content=open(image_path, 'rb').read(),
                               content_type='image/jpeg')
-
-
-class MockPhotoImages(models.Model):
-    large_img_path = Path(__file__).resolve().parent / 'test_images/2500x1500.jpg'
-    mock_large_upload = create_uploaded_file_object(large_img_path)
-    downsized_image = ImageSpecField(source='mock_large_upload',
-                                     processors=[ResizeToFit(width=500)],
-                                     format='JPEG')
-
-    small_img_path = Path(__file__).resolve().parent / 'test_images/200x100.jpg'
-    mock_small_upload = create_uploaded_file_object(large_img_path)
-    upsized_image = ImageSpecField(source='mock_small_upload',
-                                   processors=[ResizeToFit(width=500)],
-                                   format='JPEG')
 
 
 def create_photo(slug, title="Photo", description="Description", location="Location",
@@ -102,7 +85,7 @@ class PhotoAdminTests(TestCase):
     def test_get_fields_change(self):
         """Test that `thumbnail_img_tag` is included in change view `fields`"""
         photo_admin = MockPhotoAdmin()
-        fields = photo_admin.get_fields(self.request, obj=MockPhotoImages())
+        fields = photo_admin.get_fields(self.request, obj=create_photo(slug="test"))
         self.assertEqual(fields, ['large_image', 'thumbnail_img_tag', 'title', 'slug',
                                   'description', 'location', 'country', 'date_taken',
                                   'collections', 'featured', 'published'])
