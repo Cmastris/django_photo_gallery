c2a3921812ef8fb329d8d042786a01f160b60487
Chris Mastris <cmastris@hotmail.com>
2022-11-04 18:08:05 +0000

    Implement a favicon

--- photo_gallery/photo_gallery/settings.py
+++ photo_gallery/photo_gallery/settings.py
@@ -123,8 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+# Uploaded & generated files
+# https://docs.djangoproject.com/en/4.0/topics/files/
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
